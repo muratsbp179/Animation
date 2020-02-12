67417ecd49773627f9e5c09c83ab2b1090901b94
Murat Mukhametkulov <m.mukhametkulov@gmail.vom>
2020-02-12 14:19:57 +0300

    Initial commit

---
+++ app/src/main/java/com/example/animation/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.animation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        loading_text.startAnimation(inifiniteAlphaAnimation(0F, 1F, 1000L))
+    }
+
+    override fun onStop() {
+        super.onStop()
+        loading_text.animation.cancel()
+    }
+}
